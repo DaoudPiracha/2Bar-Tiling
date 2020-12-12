ec02cfa43808266eca9ecea80557666ce21a316d
Daoud Piracha <daoud.piracha@mail.mcgill.ca>
2020-12-11 22:30:43 -0500

    naive backtracking working

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import numpy as np
 
 if __name__ == '__main__':
-    m = 5
-    n = 5
+    m = 6
+    n = 6
     grid = np.zeros((m,n))
-    holes = set([(1,1), (1,2), (2,1), (2,2)])
+    res = []
+    # for i in range(6):
+    #     res.append(np.arange(10 * i, 10 * (i + 1)))
+
+    res = np.array(res)
+    holes = set([(4,3), (4,4), (4,5)])
     tetrominos = {
-        1: [(0,0), (0,1), (0,2), (0,3)],
-        -1: [(0,0), (1,0), (2,0), (3,0)],
+        1: [(0,0), (0,1), (0,2)],
+        -1: [(0,0), (1,0), (2,0)],
         # 'HF': [(0,0), (0,-1), (0,-2), (0,-3)],
         # 'VF': [(0, 0), (-1, 0), (-2, 0), (-3, 0)],
     }
@@ -20,10 +25,12 @@ if __name__ == '__main__':
         '''
         checks if given r,c lies within grid and if it is unoccupied
         '''
+
         if (r, c) in holes: return False
 
-        if r >= 0 and r < len(grid[0]) and c >= 0 and c < len(grid):
-            print ('debug', grid[r][c], search_state)
+        if r >= 0 and r < m and c >= 0 and c < n:
+            # print ('debug', r,c)
+            # print ('debug', grid[r][c], search_state)
             if grid[r][c] == search_state: return True
         return False
 
@@ -36,14 +43,16 @@ if __name__ == '__main__':
         '''
         coords = [[r + i, c + j] for i, j in tetrominos[tetromino]]
         coords_empty = [checkCoord(i, j, search_state=0.) for i, j in coords]
-        print (coords)
-        print (coords_empty)
-        print (all(coords_empty))
+        # print (coords)
+        # print (coords_empty)
+        # print (all(coords_empty))
 
-        if all(coords_empty):
+        if not all(coords_empty): return False
+        else:
             for i, j in coords:
                 grid[i][j] = tetromino
-        print (grid)
+        # print (grid)
+        return True
 
     def remove_tetronmino(tetromino, r, c, grid=grid):
         '''
@@ -65,7 +74,7 @@ if __name__ == '__main__':
         if np.sum(np.abs(grid)) == m*n - len(holes):
             print ('possible')
             return True
-        if np.sum(np.abs(grid)) %2 == 1:
+        if np.sum(np.abs(grid)) %3 != 0:
             print('not possible')
             return False
         # if containsUnreachable(grid):
@@ -74,23 +83,26 @@ if __name__ == '__main__':
         for r in range(m):
             for c in range(n):
                 if grid[r][c] == 0.:
-                    print (r,c)
+                    # print (r,c)
                     for t in tetrominos:
-                        print(r,c)
-                        print (t)
-                        place_tetronmino(t, r,c)
-                        print (grid)
+                        # print(r,c)
+                        # print (t)
+                        place_success = place_tetronmino(t, r,c)
+                        if not place_success: continue
+                        # print (grid)
                         print ('filled', np.sum(np.abs(grid)))
                         if backtrack() == True:
                             return True
                         remove_tetronmino(t,r,c)
         print ('end reached')
+        print(grid)
         return False
     print ('test')
     print (tetrominos)
 
+    # print (place_tetronmino(1, 0,0))
     print(backtrack())
-
+    print (grid)
     # (place_tetronmino(1, 0, 0))
     # print (grid)
 
